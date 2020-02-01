cf1382fab8ad84cf23bd2fc9817da3408fc78f8b
Elite9400 <60402791+Elite9400@users.noreply.github.com>
2020-02-01 17:48:41 +0100

    interfaccia base

---
+++ Controllo Personale/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Controllo_Personale
+{
+    partial class home_form
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menu_panel = new System.Windows.Forms.Panel();
+            this.end_button = new System.Windows.Forms.Button();
+            this.minimize_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // menu_panel
+            // 
+            this.menu_panel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.menu_panel.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.menu_panel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.menu_panel.Location = new System.Drawing.Point(0, 0);
+            this.menu_panel.Name = "menu_panel";
+            this.menu_panel.Size = new System.Drawing.Size(224, 547);
+            this.menu_panel.TabIndex = 0;
+            // 
+            // end_button
+            // 
+            this.end_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.end_button.BackColor = System.Drawing.Color.Coral;
+            this.end_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.end_button.Location = new System.Drawing.Point(846, 0);
+            this.end_button.Name = "end_button";
+            this.end_button.Size = new System.Drawing.Size(53, 26);
+            this.end_button.TabIndex = 1;
+            this.end_button.Text = "X";
+            this.end_button.UseVisualStyleBackColor = false;
+            this.end_button.Click += new System.EventHandler(this.end_button_Click);
+            // 
+            // minimize_button
+            // 
+            this.minimize_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.minimize_button.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.minimize_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.minimize_button.Location = new System.Drawing.Point(794, 0);
+            this.minimize_button.Name = "minimize_button";
+            this.minimize_button.Size = new System.Drawing.Size(53, 26);
+            this.minimize_button.TabIndex = 2;
+            this.minimize_button.Text = "_";
+            this.minimize_button.UseVisualStyleBackColor = false;
+            this.minimize_button.Click += new System.EventHandler(this.minimize_button_Click);
+            // 
+            // home_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(899, 546);
+            this.Controls.Add(this.minimize_button);
+            this.Controls.Add(this.end_button);
+            this.Controls.Add(this.menu_panel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "home_form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "APPLICAZIONE GESTIONE PERSONALE";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel menu_panel;
+        private System.Windows.Forms.Button end_button;
+        private System.Windows.Forms.Button minimize_button;
+    }
+}
+
---
+++ Controllo Personale/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Controllo_Personale
+{
+    public partial class home_form : Form
+    {
+        public home_form()
+        {
+            InitializeComponent();
+        }
+
+        private void end_button_Click(object sender, EventArgs e)
+        {
+            this.Close()
+        }
+
+        private void minimize_button_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Normal)
+            {
+                this.WindowState = FormWindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+    }
+}
